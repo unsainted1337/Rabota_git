f97235d0847f52e74f79e3378ae540b3e21c8125
DMitrii <bushuev.0221@gmail.com>
2021-05-04 16:14:42 +0300

    ssss

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,7 +5,7 @@ public class Main {
 
     public static void main(String[] args) {
 	 JOptionPane.showMessageDialog(null,
-             "6,12,18,24,30,36 < Текст строки> = Айфон признан лучшим китайским товаром года!",
+             "6,12,18,24,30,36  Айфон признан лучшим китайским товаром года! @Бушуев Дмитрий",
              "сообщение",JOptionPane.INFORMATION_MESSAGE);
 
     }
