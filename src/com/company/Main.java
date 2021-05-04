5a15279584ae976e8476a894c86037ea86cece9a
DMitrii <bushuev.0221@gmail.com>
2021-05-04 16:05:07 +0300

    <комментарий>

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+import javax.swing.JOptionPane;
+
+public class Main {
+
+    public static void main(String[] args) {
+	 JOptionPane.showMessageDialog(null,
+             "6,12,18,24,30,36 < Текст строки> = Айфон признан лучшим китайским товаром года!",
+             "сообщение",JOptionPane.INFORMATION_MESSAGE);
+
+    }
+}
